3e512a3d28ba8e704bb1da4ffb1fa55e08ebe332
dustinusey <dustinusey@me.com>
2023-10-05 18:25:46 -0500

    code push

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
